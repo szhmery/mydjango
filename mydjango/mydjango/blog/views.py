b61a5307eaec460a6026f85b2f49d3321ff909e1
zhaohsun <zhaohsun@cisco.com>
2018-05-29 18:02:49 +0800

    add templates

--- mydjango/mydjango/blog/views.py
+++ mydjango/mydjango/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,Context
+from django.shortcuts import render_to_response
 # Create your views here.
 
+# def index(req):
+#     t = loader.get_template("index.html")
+#     c = Context({})
+#
+#     return HttpResponse(t.render(c))
+
 def index(req):
-    return HttpResponse('<h1>Hello welcome to django!</h1>')
+    return render_to_response('index.html',{})
