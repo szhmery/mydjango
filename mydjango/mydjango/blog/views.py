1d09bc6ef189180ee36e7bbfb4b422d34be012bf
zhaohsun <zhaohsun@cisco.com>
2018-05-31 17:43:19 +0800

    add template tag/url

--- mydjango/mydjango/blog/views.py
+++ mydjango/mydjango/blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader,Context
 from django.shortcuts import render_to_response
+
+
 # Create your views here.
 
 # def index(req):
@@ -9,6 +8,18 @@ from django.shortcuts import render_to_response
 #     c = Context({})
 #
 #     return HttpResponse(t.render(c))
+class Person(object):
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
 
-def index(req):
-    return render_to_response('index.html',{})
+    def say(self):
+        return "I am "+self.name
+
+
+def index(req, parm):
+    user = {'name':'tom','age':23,'sex':'female'}
+    #user = Person('Tom', 23, 'male')
+    book_list = ['python', 'java', 'php', 'web']
+    return render_to_response('index.html', {'title': 'my page',  'book_list': book_list,'user': user, 'id':parm})
--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+#from blog.views import index
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('blog.views',
     # Examples:
     # url(r'^$', 'mydjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/index/$','blog.views.index'),
+    #url(r'^blog/index/$','blog.views.index'),
+    #url(r'^blog/index/$', index),
+    url(r'^blog/index/(\d{2})/$', 'index'),
 )
